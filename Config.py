7834c121b7fb557f335552cb219d35a9bc3018f5
aaaaaaaaaaabbbkakoa <72983712+aaaaaaaaaaabbbkakoa@users.noreply.github.com>
2020-10-16 23:28:37 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -22,10 +22,15 @@ class Config():
 
 class Messages():
       HELP_MSG = [
-        "p",
+        ".",
 
-       
-        "**bot rusak**"
+        "**Force Subscribe**\n__Force group members to join a specific channel before sending messages in the group.\nI will mute members if they not joined your channel and tell them to join the channel and unmute themself by pressing a button.__",
+        
+        "**Setup**\n__First of all add me in the group as admin with ban users permission and in the channel as admin.\nNote: Only creator of the group can setup me and i will leave the chat if i am not an admin in the chat.__",
+        
+        "**Commmands**\n__/ForceSubscribe - To get the current settings.\n/ForceSubscribe no/off/disable - To turn of ForceSubscribe.\n/ForceSubscribe {channel username} - To turn on and setup the channel.\n/ForceSubscribe clear - To unmute all members who muted by me.\n\nNote: /FSub is an alias of /ForceSubscribe__",
+        
+        "**Developed by @viperadnan**"
       ]
 
-      START_MSG = "**ha [{}](tg://user?id={})**\n__error"
+      START_MSG = "**Hey [{}](tg://user?id={})**\n__I can force members to join a specific channel before writing messages in the group.\nLearn more at /help__"

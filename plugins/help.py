02e93b694c6b1907c3b44f7b0548fa3f7faae8f7
aaaaaaaaaaabbbkakoa <72983712+aaaaaaaaaaabbbkakoa@users.noreply.github.com>
2020-10-16 21:33:32 +0700

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -42,9 +42,9 @@ def map(pos):
             [InlineKeyboardButton(text = '-->', callback_data = "help+2")]
         ]
     elif(pos==len(tr.HELP_MSG)-1):
-        url = "https://github.com/viperadnan-git/force-subscribe-telegram-bot/issues/new"
+        url = "https://t.me/gruproleplaymap"
         button = [
-            [InlineKeyboardButton(text = 'Support Chat', url="https://t.me/ViperCommunity")],
+            [InlineKeyboardButton(text = 'Support Chat', url="https://t.me/roleplaysqpromote")],
             [InlineKeyboardButton(text = 'Feature Request & Issues', url=url)],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
         ]

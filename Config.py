701e1c3cb8cd99d883181144e4ea50d7e0e5fb5a
yuiokjgft <73433100+yuiokjgft@users.noreply.github.com>
2020-10-30 01:23:51 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -12,7 +12,7 @@ class Config():
     SUDO_USERS = list(set(SUDO_USERS))
   else:
     BOT_TOKEN = "1070013851:AAFOZo76ZzpfKGOuCJ8bKBmPkP-2FOhJWeQ"
-    DATABASE_URL = "postgres://ilnpklfnmolbdw:5c45f41fb64a2d46f7890af8c8454d101cc3e286c257078d184a68621f488f18@ec2-52-44-235-121.compute-1.amazonaws.com:5432/dc4l0h6oida2mo"
+    DATABASE_URL = "postgres://ilnpklfnmolbdw:5c45f41fb64a2d46f7890af8c8454d101cc3e286c257078d184a68621f488f18@ec2-52-44-235-121.compute-1.amazonaws.com:5432/dc4l0h6oida2mooida2mo"
     APP_ID = "1948374"
     API_HASH = "113be69e13bf6f71583fb0783b1ab841"
     SUDO_USERS = list(set(int(x) for x in ''.split()))
